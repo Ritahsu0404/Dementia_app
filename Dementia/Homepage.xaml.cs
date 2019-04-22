3394d476ca083ef7cd51c12d881f54eddd444a9d
Ritahsu <ritahsu@emotibot.com>
2019-04-22 14:38:29 +0800

    update user login

---
+++ Dementia/Homepage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Dementia
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "回到首頁");
+
+        }
+
+    }
+}
---
+++ Dementia/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Dementia
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "回登入頁面");
+            buttonLogin.Clicked += ButtonLogin_Clicked;
+        }
+
+        private async void ButtonLogin_Clicked(object sender, EventArgs e)
+        {
+            // 換頁
+            await Navigation.PushAsync(new LoginPage(),true);
+        }
+    }
+}
